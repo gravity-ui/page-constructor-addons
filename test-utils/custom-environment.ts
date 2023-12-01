40e84639a3757d19d4b5e2c3399d66294176d72b
gorgeousvlad <gorgeousvlad@gmail.com>
2023-12-01 12:09:43 +0100

    fix: tests

---
+++ test-utils/custom-environment.ts
@@ -0,0 +1,21 @@
+import {EnvironmentContext, JestEnvironmentConfig} from '@jest/environment';
+import JsDomEnvironmetn from 'jest-environment-jsdom';
+
+const matchMedia = (query: string) =>
+    ({
+        query,
+        matches: false,
+        addListener: function () {},
+        removeListener: function () {},
+    } as unknown as MediaQueryList);
+
+class CustomEnvironment extends JsDomEnvironmetn {
+    constructor(config: JestEnvironmentConfig, context: EnvironmentContext) {
+        super(config, context);
+        this.global.matchMedia = matchMedia;
+        this.global.HTMLMediaElement.prototype.pause = () => {};
+        this.global.HTMLMediaElement.prototype.play = () => Promise.resolve();
+    }
+}
+
+export default CustomEnvironment;
--- test-utils/setup-tests.ts
+++ test-utils/setup-tests.ts
@@ -1,9 +1,11 @@
-import {configure} from '@testing-library/dom';
+import {configure as configureDOM} from '@testing-library/dom';
+import {configure as configureReact} from '@testing-library/react';
 
-import {Lang, configure as libConfigure} from '../src';
+import {Lang, configure as libConfigure} from '../src/utils/configure';
 
 libConfigure({
     lang: Lang.En,
 });
 
-configure({testIdAttribute: 'data-qa'});
+configureDOM({testIdAttribute: 'data-qa'});
+configureReact({testIdAttribute: 'data-qa'});
